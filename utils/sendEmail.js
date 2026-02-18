73ee137caee02c592738ccaf5a989fccc1154f78
GhidionAdrian <ghidion.adrian@elev.cihcahul.md>
2026-02-18 23:04:46 +0200

    fix: diagnosticare si verificare trimitere email backend

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,9 +3,7 @@ require("dotenv").config();
 
 // Configurare transporter
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
-  secure: false, // true pentru 465, false pentru alte porturi
+  service: "gmail",
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
